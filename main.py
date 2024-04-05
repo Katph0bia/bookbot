eac0f13be4440e085028f98e1a65827b452f895e
Katph0bia <alexandrejadc@gmail.com>
2024-04-05 01:47:33 +0000

    Added a letter occurence function to main.py

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ with open("books/frankenstein.txt") as f:
     counter = 0
     for word in words:
         counter += 1
-    print(counter)
+    print(f"This book has >>{counter}<< words.")
+
+# Definition to count the occurence of letters in the book
+def letter_count():
+    # 1) Read book; 2) Dictionary; 3) Split Book; 4) Count Everything.
+    
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        letters_dictionary =  {letter: 0 for letter in alphabet}
+
+        for char in file_contents.lower():
+            if char in alphabet:
+                letters_dictionary[char] += 1
+    return (letters_dictionary)
+print (letter_count())
+
 
 # main()
